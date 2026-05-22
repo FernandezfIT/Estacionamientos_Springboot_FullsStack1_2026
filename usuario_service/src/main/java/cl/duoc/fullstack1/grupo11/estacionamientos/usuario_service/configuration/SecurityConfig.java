16f592c3205475d82899663cea4bd3774bed0c93
FernandezfIT <feli.fernandezm@duocuc.cl>
2026-05-22 01:36:10 -0400

    Integracion usario_service -> auth_service

---
+++ usuario_service/src/main/java/cl/duoc/fullstack1/grupo11/estacionamientos/usuario_service/configuration/SecurityConfig.java
@@ -0,0 +1,36 @@
+package cl.duoc.fullstack1.grupo11.estacionamientos.usuario_service.configuration;
+
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf(csrf -> csrf.disable())
+
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/v1/usuarios/**").permitAll()
+                        .requestMatchers("/api/v1/roles/**").permitAll()
+                        .anyRequest().denyAll()
+                )
+
+                .formLogin(form -> form.disable())
+
+                .httpBasic(httpBasic -> httpBasic.disable());
+
+        return http.build();
+    }
+}
