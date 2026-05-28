7cfa95fbdb343ca8a95031923a72a512e0725d85
NovoaCl <cl.novoa.ira@gmail.com>
2026-05-28 01:10:47 -0400

    Integration with movimiento_service

--- liberacion_service/src/main/java/cl/duoc/fullstack1/grupo11/estacionamientos/liberacion_service/controller/LiberacionController.java
+++ liberacion_service/src/main/java/cl/duoc/fullstack1/grupo11/estacionamientos/liberacion_service/controller/LiberacionController.java
@@ -2,12 +2,14 @@ package cl.duoc.fullstack1.grupo11.estacionamientos.liberacion_service.controlle
 
 import java.util.List;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -38,9 +40,10 @@ public class LiberacionController {
 
     @PostMapping
     public ResponseEntity<LiberacionResponse> liberarPlaza(
-            @Valid @RequestBody LiberacionRequest request
+            @Valid @RequestBody LiberacionRequest request,
+            @RequestHeader(HttpHeaders.AUTHORIZATION) String authorizationHeader
     ) {
-        LiberacionResponse liberacionCreada = liberacionService.liberarPlaza(request);
+        LiberacionResponse liberacionCreada = liberacionService.liberarPlaza(request, authorizationHeader);
         return ResponseEntity.status(HttpStatus.CREATED).body(liberacionCreada);
     }
 }
